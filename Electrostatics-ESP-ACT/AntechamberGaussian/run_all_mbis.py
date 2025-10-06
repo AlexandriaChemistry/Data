4f03c877ae0413ad0634948f2b0e16420d757efc
David van der Spoel <davidvanderspoel@gmail.com>
2025-10-06 23:02:09 +0200

    New inputs and scrips

---
+++ Electrostatics-ESP-ACT/AntechamberGaussian/run_all_mbis.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+from run_gaussian import get_mols
+
+def run_it(compound:str, charge:int, basis:str):
+    os.chdir(compound)
+    method = "MP2"
+    if not (os.path.exists("scf.json") and os.path.exists("CCSD.json")):
+        os.system(f"sbatch ../run_mbis.py {compound} {charge} {method} {basis}")
+    os.chdir("..")
+
+if __name__ == "__main__":
+    mols = get_mols()
+    os.chdir("MBIS")
+    for mol in mols:
+        os.makedirs(mol, exist_ok=True)
+        run_it(mol, mols[mol]["q"], mols[mol]["basis"])
