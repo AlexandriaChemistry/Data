7a218c72cd67abca52137742e6207cbbbd667082
David van der Spoel <david.vanderspoel@gmail.com>
2025-10-08 13:39:43 +0200

    Small updates

--- Electrostatics-ESP-ACT/Scripts4Figs/ESP-ions.py
+++ Electrostatics-ESP-ACT/Scripts4Figs/ESP-ions.py
@@ -15,6 +15,7 @@ with open(input_json, 'r') as json_f:
     output_data = json.load(json_f)
 
 start = 80
+delta = 5
 
 xref = output_data["data"]["Br"][0]
 
@@ -31,39 +32,44 @@ tempdir = "vtmp"
 os.makedirs(tempdir, exist_ok=True)
 os.chdir(tempdir)
 
+def labels(outf):
+    outf.write('@ xaxis label "Distance ($\\mathrm{\\AA}$)"\n')
+    outf.write('@ yaxis label "ESP (kJ/mol e)"\n')
+
 with open("minus.xvg", "w") as f_minus:
-    for i in range(start, len(xref)):
+    labels(f_minus)
+    for i in range(start, len(xref), delta):
         f_minus.write(f"{xref[i]:10.4f}  {-pc[i]:10.6f}\n")
 
 with open("plus.xvg", "w") as f_plus:
-    for i in range(start, len(xref)):
+    labels(f_plus)
+    for i in range(start, len(xref), delta):
         f_plus.write(f"{xref[i]:10.4f}  {pc[i]:10.6f}\n")
 
 for ion, xy in output_data["data"].items():
     if len(xy) == 2 and len(xy[0]) == len(xy[1]):
         pname = f"{ion}-absolute.xvg"
         with open(pname, "w") as outf:
-            outf.write('@ xaxis label "Distance ($\\mathrm{\\AA}$)"\n')
-            outf.write('@ yaxis label "ESP (kJ/mol e)"\n')
-            for i in range(start, len(xy[0])):
+            labels(outf)
+            for i in range(start, len(xy[0]), delta):
                 outf.write(f"{xy[0][i]:10.4f}  {xy[1][i]:10.6f}\n")
 
 opts = (
-    "-mk -xframe 12 -yframe 12 -noshow "
-    "-legend_x 0.5 -legend_y 0.95 "
+    "-mk o x + . -xframe 12 -yframe 12 -noshow "
+    "-legend_x 0.5 -legend_y 0.8 "
     "-alfs 48 -tickfs 48 -lfs 38 "
-    "-colors crimson lightcoral royalblue mediumseagreen"
+#    "-colors crimson lightcoral royalblue mediumseagreen"
 )
 
 os.system(
     f"viewxvg -f F-absolute.xvg Cl-absolute.xvg Br-absolute.xvg minus.xvg "
     f"-label Fluoride Chloride Bromide PC "
-    f"-pdf ../anion-esp-absolute.pdf {opts}"
+    f"-save ../anion-esp-absolute.pdf {opts}"
 )
 
 os.system(
     f"viewxvg -f Li-absolute.xvg Na-absolute.xvg K-absolute.xvg plus.xvg "
     f"-label Lithium Sodium Potassium PC "
-    f"-pdf ../cation-esp-absolute.pdf {opts}"
+    f"-save ../cation-esp-absolute.pdf {opts}"
 )
 
