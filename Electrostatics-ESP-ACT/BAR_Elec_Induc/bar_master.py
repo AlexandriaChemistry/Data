cdf5c8b6fde1d82256bf5d5471fc5759b9840212
David van der Spoel <david.vanderspoel@gmail.com>
2025-12-11 16:42:43 +0100

    Updated scripts

--- Electrostatics-ESP-ACT/BAR_Elec_Induc/bar_master.py
+++ Electrostatics-ESP-ACT/BAR_Elec_Induc/bar_master.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
+lw    = 0.3
+legfs = 28
+#colors = [ "#5e3c99", "#c8f7c5", "#ffb000", "#0000cc", "#e41a1c", "#e41a1c" ]
+colors  = [ "#aaaaaa", "#00cc00",  "#00eeee", "#0000cc", "#ffff00", "#ff0000" ]
+hatches = [ '+', '', '-', '', 'x', '', 'O', '.', '*', '\\' ]
+
 def readLATEX(filepath):
     labels, rows = [], []
     with open(filepath, "r") as f:
@@ -29,31 +35,38 @@ def plot_table4(texfile):
     r, SAPT, TIP4P, MBIS, CHARMM, GV4, GS4 = data.T
     ions_display = ["Li⁺–H₂O", "Na⁺–H₂O", "K⁺–H₂O", "F⁻–H₂O", "Cl⁻–H₂O", "Br⁻–H₂O", "H₂O–H₂O"]
     x = np.arange(len(ions_display))
+    mydata = [ { "data": SAPT,  "label": "SAPT2+(CCD)-δMP2" },
+               { "data": TIP4P,  "label": "PC/TIP4P-Ew" },
+               { "data": MBIS,   "label": "MBIS-S" },
+               { "data": CHARMM, "label": "CHARMM Drude" },
+               { "data": GV4,    "label": "PC+GV4" },
+               { "data": GS4,    "label": "PC+GS4" } ]
+    
     width = 0.12
 
-    plt.figure(figsize=(18, 8), dpi=300)
+    plt.figure(figsize=(12, 15), dpi=300)
 
-    plt.bar(x - 3*width, SAPT, width, label="SAPT2+(CCD)-δMP2", color="#5e3c99", edgecolor="black", linewidth=5)
-    plt.bar(x - 2*width, TIP4P, width, label="TIP4P-Ew",       color="#c8f7c5", edgecolor="black", linewidth=0.7)
-    plt.bar(x - width,   MBIS, width, label="MBIS-S",          color="#ffb000", edgecolor="black", linewidth=0.7)
-    plt.bar(x,           CHARMM, width, label="CHARMM Drude",  color="#c9b7ff", edgecolor="black", linewidth=0.7)
-    plt.bar(x + width,   GV4, width, label="PC+GV4",           color="#e41a1c", edgecolor="black", linewidth=5)
-    plt.bar(x + 2*width, GS4, width, label="PC+GS4",           color="#00a9cf", edgecolor="black", linewidth=5)
+    x0 = x - 3*width
+    i  = 0
+    for md in mydata:
+        plt.barh(x0, md["data"],  width, label=md["label"], color=colors[i], edgecolor="black", linewidth=lw, hatch=hatches[i])
+        x0 += width
+        i += 1
 
-    plt.xticks(x, ions_display, fontsize=30, rotation=20)
-    plt.yticks(fontsize=30)
-    plt.ylabel("Electrostatic Energy (kJ/mol)", fontsize=30)
-    plt.axhline(0, color="black", linewidth=1.2)
-    plt.grid(axis="y", linestyle="--", alpha=0.25)
+    plt.yticks(x, ions_display, fontsize=30, rotation=20)
+    plt.xticks(fontsize=20)
+    plt.xlabel("Electrostatic Energy (kJ/mol)", fontsize=30)
+#    plt.axhline(0, color="black", linewidth=1.2)
+    plt.grid(axis="x", linestyle="--", alpha=0.25)
 
     ax = plt.gca()
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
 
-    plt.legend(fontsize=20, frameon=False, ncol=3, bbox_to_anchor=(0.5, -0.22), loc="upper center")
+    plt.legend(fontsize=legfs, frameon=False, ncol=2, bbox_to_anchor=(0.5, -0.07), loc="upper center")
     plt.tight_layout()
     plt.subplots_adjust(bottom=0.05, left=0.05)
-    plt.savefig("table4.pdf", dpi=600, bbox_inches="tight")
+    plt.savefig("elec-water-ions.pdf", dpi=600, bbox_inches="tight")
     plt.close()
 
 
@@ -63,21 +76,27 @@ def plot_table6(texfile):
     ions_display = ["Li⁺–F⁻", "Li⁺–Cl⁻", "Li⁺–Br⁻", "Na⁺–F⁻", "Na⁺–Cl⁻", "Na⁺–Br⁻", "K⁺–F⁻", "K⁺–Cl⁻", "K⁺–Br⁻"]
     x = np.arange(len(ions_display))
     width = 0.13
+    mydata = [ { "data": SAPT,   "label": "SAPT2+(CCD)-δMP2" },
+               { "data": PC,     "label": "PC/TIP4P-Ew" },
+               { "data": MBIS,   "label": "MBIS-S" },
+               { "data": Walz,   "label": "Walz2018" },
+               { "data": GV4,    "label": "PC+GV4" },
+               { "data": GS4,    "label": "PC+GS4" } ]
+    
+    plt.figure(figsize=(12, 15), dpi=300)
 
-    plt.figure(figsize=(18, 7), dpi=300)
+    x0 = x - 2.5*width
+    i  = 0
+    for md in mydata:
+        plt.barh(x0, md["data"],  width, label=md["label"], color=colors[i], edgecolor="black", linewidth=lw, hatch=hatches[i])
+        x0 += width
+        i += 1
 
-    plt.bar(x - 2.5*width, SAPT, width, label="SAPT2+(CCD)-δMP2", color="#5e3c99", edgecolor="black", linewidth=5)
-    plt.bar(x - 1.5*width, PC,   width, label="PC",               color="#c9b7ff", edgecolor="black", linewidth=1)
-    plt.bar(x - 0.5*width, MBIS, width, label="MBIS-S",           color="#ffb000", edgecolor="black", linewidth=1)
-    plt.bar(x + 0.5*width, Walz, width, label="Walz Model",       color="#c6f9e3", edgecolor="black", linewidth=1)
-    plt.bar(x + 1.5*width, GV4,  width, label="PC+GV4",           color="#e31a1c", edgecolor="black", linewidth=5)
-    plt.bar(x + 2.5*width, GS4,  width, label="PC+GS4",           color="#00a9cf", edgecolor="black", linewidth=5)
-
-    plt.xticks(x, ions_display, fontsize=30, rotation=20)
-    plt.yticks(fontsize=30)
-    plt.ylabel("Electrostatic Energy (kJ/mol)", fontsize=30)
-    plt.axhline(0, color="black", linewidth=1.2)
-    plt.grid(axis="y", linestyle="--", alpha=0.25)
+    plt.yticks(x, ions_display, fontsize=30, rotation=20)
+    plt.xticks(fontsize=20)
+    plt.xlabel("Electrostatic Energy (kJ/mol)", fontsize=30)
+#    plt.axhline(0, color="black", linewidth=1.2)
+    plt.grid(axis="x", linestyle="--", alpha=0.25)
 
     ax = plt.gca()
     ax.spines["bottom"].set_linewidth(1.8)
@@ -85,10 +104,10 @@ def plot_table6(texfile):
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
 
-    plt.legend(fontsize=20, frameon=False, ncol=3, bbox_to_anchor=(0.5, -0.25), loc="upper center")
+    plt.legend(fontsize=legfs, frameon=False, ncol=2, bbox_to_anchor=(0.5, -0.07), loc="upper center")
     plt.tight_layout()
     plt.subplots_adjust(bottom=0.05, left=0.05)
-    plt.savefig("table6.pdf", dpi=1200, bbox_inches="tight")
+    plt.savefig("elec-ions.pdf", dpi=1200, bbox_inches="tight")
     plt.close()
 
 
@@ -102,9 +121,9 @@ def plot_table5(texfile):
 
     plt.figure(figsize=(14, 6), dpi=300)
 
-    plt.bar(x - width, SAPT, width, label="SAPT2+(CCD)-δMP2", color="#5e3c99", edgecolor="black", linewidth=5)
-    plt.bar(x,        CHARMM, width, label="CHARMM Drude",     color="#e31a1c", edgecolor="black", linewidth=1)
-    plt.bar(x + width, GS4,   width, label="PC+GS4",            color="royalblue", edgecolor="black", linewidth=5)
+    plt.bar(x - width, SAPT, width, label="SAPT2+(CCD)-δMP2", color=colors[0], edgecolor="black", linewidth=lw, hatch=hatches[0])
+    plt.bar(x,        CHARMM, width, label="CHARMM Drude",    color=colors[3], edgecolor="black", linewidth=lw, hatch=hatches[3])
+    plt.bar(x + width, GS4,   width, label="PC+GS4",          color=colors[5], edgecolor="black", linewidth=lw, hatch=hatches[5])
 
     plt.xticks(x, ions_display, fontsize=30, rotation=20)
     plt.yticks(fontsize=30)
@@ -118,9 +137,9 @@ def plot_table5(texfile):
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
 
-    plt.legend(fontsize=20, frameon=False)
+    plt.legend(fontsize=legfs, frameon=False)
     plt.tight_layout()
-    plt.savefig("table5.pdf", dpi=1200, bbox_inches="tight")
+    plt.savefig("induc-water-ions.pdf", dpi=1200, bbox_inches="tight")
     plt.close()
 
 
@@ -128,5 +147,4 @@ if __name__ == "__main__":
     plot_table4("ion-water-SAPT2-TIP4Pew-ACT4S.tex")
     plot_table6("Ions-sapt2-JC-Walz2018a-ACT.tex")
     plot_table5("ion-water-induction.tex")
-    print("figs generated: table4.pdf, table6.pdf, table5.pdf")
 
--- Electrostatics-ESP-ACT/Scripts4Figs/Electrostatic_4models.py
+++ Electrostatics-ESP-ACT/Scripts4Figs/Electrostatic_4models.py
@@ -139,7 +139,7 @@ def print_tex():
 \\centering
 \\includegraphics[width=0.98\\linewidth]{Figures/SAPT_%s_100.pdf}
 \\end{minipage}
-\\caption{Electrostatic energies from SAPT0 with the aug-cc-pVTZ basis set and different charge models based on fitting the ESP from 2.0 to 4.5 {\\AA} (left) and 0.0 to 4.5 {\\AA} (right) for %s. Note that units on the y-axis may differ between plots. RMSD is computed for the points plotted, but it should be noted the range of points does not correspond to the range used for fitting, for those numbers please see Tables S1 and S2.}
+\\caption{Electrostatic energies from SAPT0 with the aug-cc-pVTZ basis set and {\\bf four different charge models} based on fitting the ESP from 2.0 to 4.5 {\\AA} (left) and 0.0 to 4.5 {\\AA} (right) for %s. Note that units on the y-axis may differ between plots. RMSD is computed for the points plotted, but it should be noted the range of points does not correspond to the range used for fitting, for those numbers please see Tables S1 and S2.}
 \\label{fig:pot_%s}
 \\end{figure}
 """ % ( ionpair, ionpair, ionpair, ionpair ) )
