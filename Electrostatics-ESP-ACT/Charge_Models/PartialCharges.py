0b49c3f1948984dd86cdb54efbf77ac2847fdcdd
David van der Spoel <david.vanderspoel@gmail.com>
2025-12-11 13:04:49 +0100

    Fixed script for fetching charges

--- Electrostatics-ESP-ACT/Charge_Models/PartialCharges.py
+++ Electrostatics-ESP-ACT/Charge_Models/PartialCharges.py
@@ -16,22 +16,23 @@ compounds_of_interest = [
     "guanidinium",
     "imidazolium",
     "water",
-    "lithium-ion",
-    "sodium-ion",
-    "potassium-ion",
+    "lithium",
+    "sodium",
+    "potassium",
     "fluoride",
     "chloride",
     "bromide"
     ]
 
-log_files = { "ESP":        { "label": "ESP", "ncol": 1 },
-              "PC-elec":    { "label": "PC$_{e}$", "ncol": 1 },
-              "PC-allelec": { "label": "PC$_{ei}$", "ncol": 1 },
-              "MBIS-S":     { "label": "MBIS-S", "ncol": 2 },
-              "PC+SV-elec": { "label": "PC+SV4$_{e}$", "ncol": 2 },
-              "PC+GV-elec": { "label": "PC+GV4$_{e}$", "ncol": 2 },
-              "PC+GS-elec": { "label": "PC+GS4$_{e,i}$", "ncol": 2 },
-              }
+log_files = {
+    "ESP":        { "label": "ESP", "ncol": 1 },
+    "PC-elec":    { "label": "PC$_{e}$", "ncol": 1 },
+    "PC-allelec": { "label": "PC$_{ei}$", "ncol": 1 },
+    "MBIS-S":     { "label": "MBIS-S", "ncol": 2 },
+    "PC+SV-elec": { "label": "PC+SV4$_{e}$", "ncol": 2 },
+    "PC+GV-elec": { "label": "PC+GV4$_{e}$", "ncol": 2 },
+    "PC+GS-elec": { "label": "PC+GS4$_{e,i}$", "ncol": 2 },
+}
 
 def simplify(atype:str)->str:
     newrep = { "hn": [ "hna1", "hna2", "hne1", "hne2", "hne3",
@@ -63,7 +64,13 @@ def simplify(atype:str)->str:
                "n4_s": [  "v1n4", "v1n4a", "v1n4e", "v1n4m" ],
                "o2": [ "o2f", "o2a1", "o2a2" ],
                "o2_s": [ "v1o2", "v1o2f", "v1o2a1", "v1o2a2" ],
-               "ow_s": [ "v1ow" ]
+               "ow_s": [ "v1ow" ],
+               "Li+_s": [ "v1Li+" ],
+               "Na+_s": [ "v1Na+" ],
+               "K+_s": [ "v1K+" ],
+               "F-_s": [ "v1F-" ],
+               "Cl-_s": [ "v1Cl-" ],
+               "Br-_s": [ "v1Br-" ]
               }
     for key in newrep:
         if atype in newrep[key]:
@@ -80,12 +87,13 @@ def get_zeta_mbiss(compound:str):
         zzz   = "valence_widths"
         if atoms in data and zzz in data:
             for a in range(len(data[atoms])):
-                zeta.append( { "atom": data[atoms][a], "zeta": 1/(2*BOHR*float(data[zzz][a][0])) })
-                zeta.append( { "atom": data[atoms][a]+"_s", "zeta": 0 })
+                name = data[atoms][a]
+                zeta.append({ "type": name, "zeta": 1/(2*BOHR*float(data[zzz][a][0])) })
+                zeta.append({ "type": name + "_s", "zeta": 0 })
     return zeta
 
 def get_zeta(model:str, compound:str):
-    zeta = []
+    zeta = {}
     bcc  = []
     eem  = []
     if log_files[model]["ncol"] == 2:
@@ -113,8 +121,10 @@ def get_zeta(model:str, compound:str):
                                                 ptype = entry[param]["@type"]
                                                 value = entry[param][val]
                                                 if mytype == "COULOMB":
-                                                    idl   = entry[ident].lower()[:-2]
-                                                    zeta.append({ "atom": idl, "zeta": float(value) })
+                                                    idl = entry[ident][:-2]
+                                                    if not model == "MBIS-S":
+                                                        idl = simplify(idl)
+                                                    zeta[idl] = float(value)
                                                 elif mytype == "BONDCORRECTIONS":
                                                     if not entry[ident] in bcc:
                                                         bcc[entry[ident]] = {}
@@ -124,7 +134,7 @@ def get_zeta(model:str, compound:str):
                                                         eem[entry[ident]] = {}
                                                     eem[entry[ident]][ptype] = value
                         
-    return zeta, eem, bcc
+    return zeta#, eem, bcc
         
 def extract_data_from_log():
     data = {}
@@ -168,13 +178,17 @@ def extract_data_from_log():
                         columns = line.split()
                         if len(columns) > 4 and columns[0].isdigit():
                             aindex     = 1
-                            atom_type  = simplify(columns[2])
+                            atom_type  = columns[2]
+                            if not file_type == "MBIS-S":
+                                atom_type = simplify(atom_type)
                             if not (guanidinium_hack and "w" in atom_type ):
                                 acm_value  = columns[3]
                                 for mypart in data[file_type][current_compound]:
                                     if mypart["type"] == atom_type:
                                         aindex   += 1
-                                        atom_type = simplify(columns[2])
+                                        atom_type  = columns[2]
+                                        if not file_type == "MBIS-S":
+                                            atom_type = simplify(atom_type)
                                 data[file_type][current_compound].append( { "type": atom_type, "aindex": aindex, "value": acm_value } )
             if debug:
                 print(f"file {file_type} data {data[file_type]['guanidinium']}")
@@ -201,7 +215,7 @@ def save_data_as_latex(data):
         for compound in compounds_of_interest:
             file.write("\\begin{sidewaystable}\n")
             file.write("\\centering\n")
-            file.write(r"\caption{Partial charges q (e) and screening widths $\zeta$ (1/nm) for " + compound + " from ESP, MBIS-S and ACT models. First line, atom, second line shell or virtual site.}")
+            file.write(r"\caption{Partial charges q (e) and screening widths $\zeta$ (1/nm) for " + compound + " from ESP, MBIS-S and ACT models. First line, atom, second line shell or virtual site. $\\zeta$ = 0 indicates a point charge is used.}")
             file.write("\n")
             file.write("\\label{tab:q%s}\n" % compound)
             file.write("\\begin{tabular}{l")
@@ -235,54 +249,50 @@ def save_data_as_latex(data):
                 maxpart = npart
             else:
                 maxpart = int(npart/2)
+            PCGS = "PC+GS-elec"
             for ipart in range(maxpart):
-                particle = data["PC+GS-elec"][compound][ipart]
-                mystr    = particle["type"].replace("_", "\\_")
+                # Assume PC+GC has all particles
+                particle = data[PCGS][compound][ipart]
+                ptype    = particle["type"]
+                mystr    = ptype.replace("_", "\\_")
                 for method in data.keys():
                     if not compound in data[method]:
                         sys.exit("Could not find compound %s for method %s" % ( compound, method ))
+                    qval = None
+                    zval = None
+                    if method == PCGS or True:
+                        ptype = particle['type']
+                    else:
+                        bases = particle['type'].split('_')
+                        ptype = bases[0]
+                        if len(bases) > 1:
+                            if not bases[0].startswith('v'):
+                                ptype = "v1"+bases[0]
                     try:
-                        if method in zeta:
-                            natom = len(data[method][compound])
-                            fval = None
-                            if ipart < natom and particle["type"] == data[method][compound][ipart]["type"]:
-                                fval = float(data[method][compound][ipart]["value"])
-                            if fval:
-                                total[method] += fval
-                                mystr += ( " & %s" % str(round(fval, 4)) )
-                            else:
-                                mystr += ( " & " )
-                            zval = None
-                            # Hardcoding stuff, sigh.
-                            if method == "MBIS-S":
-                                if method in zeta and compound in zeta[method] and zeta[method][compound] and ipart < len(zeta[method][compound]):
-                                    zval = zeta[method][compound][ipart]["zeta"]
-                            else:
-                                if method == "PC+GS-elec":
-                                    ppps = particle['type'].split('-')
-                                    # To make sure the minus sign in ions is maintained
-                                    ppp = ppps[0].lower()
-                                    for k in range(1,len(ppps)-1):
-                                        ppp += "-" + ppps[k]
-                                else:
-                                    ppp = "v1"+particle['type'].split('_')[0].lower()
-                                for j in range(len(zeta[method][compound][0])):
-                                    if zeta[method][compound][0][j]['atom'] == ppp:
-                                        zval = zeta[method][compound][0][j]['zeta']
-                                        break
-
-#                            elif particle in zeta[method][compound]:
-#                                zval = zeta[method][compound][particle]
-                            if zval:
-                                mystr += (" & %s" % ( str(round(zval, 2)) ))
-                            else:
-                                mystr += " & - "
-                        elif particle["type"] == data[method][compound][int(ipart/2)]["type"]:
-                            fval = float(data[method][compound][int(ipart/2)]["value"])
-                            total[method] += fval
-                            mystr += ( " & %s " %  str(round(fval, 4)) )
+                        if method == "MBIS-S":
+                            if compound in zeta[method] and ipart < len(zeta[method][compound]):
+                                qval = float(data[method][compound][ipart]["value"])
+                                zval = zeta[method][compound][ipart]["zeta"]
                         else:
-                            mystr += " & "
+                            for j in range(min(maxpart,len(data[method][compound]))):
+                                if (ptype == data[method][compound][j]["type"] and
+                                    particle["aindex"] == data[method][compound][j]["aindex"]):
+                                    qval = float(data[method][compound][j]["value"])
+                        if qval:
+                            total[method] += qval
+                            mystr += ( " & %s" % str(round(qval, 4)) )
+                        else:
+                            mystr += ( " & " )
+                        if method in zeta and not method == "MBIS-S":
+                            if compound in zeta[method] and ptype in zeta[method][compound]:
+                                zval = zeta[method][compound][ptype]
+                            else:
+                                sys.exit("Cannot find zeta for particle %s in compound %s method %s" %
+                                         ( particle, compound, method ) )
+
+                        if zval != None:
+                            mystr += (" & %s" % ( str(round(zval, 2)) ))
+
                     except ValueError:
                         sys.exit("Don't know how to treat charge %s for type %s" % ( mypart["value"], mypart["type"] ))
                     
@@ -297,7 +307,7 @@ def save_data_as_latex(data):
             file.write("\\end{tabular}\n")
             file.write("\\end{sidewaystable}\n")
 
-    print("LaTeX file generated successfully!")
+    print("LaTeX file %s generated successfully!" % combined_output_file)
 
 
 data = extract_data_from_log()
